40967c3ae088b42fb6312c2471ef5b62d37efda1
Shivaprasad <svbneelmane@gmail.com>
2019-05-27 12:55:47 +0530

    Commiting first file

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @author sbhat1
+ */
+import java.io.*;
+public class Hello {
+    public static void main(String[] args) {
+        //Hello
+        System.out.println("Hello");
+    }
+    
+}
